9ac8980d76ce8cc260c9d6e709bfba9aa029b8f0
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-03-08 15:19:39 +0200

    add: some fragments

--- app/src/main/java/fi/jessestenroth/koskaelokuvat/fragments/ListFragment.java
+++ app/src/main/java/fi/jessestenroth/koskaelokuvat/fragments/ListFragment.java
@@ -4,14 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import fi.jessestenroth.koskaelokuvat.R;
 
 public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceBundle){
-        Button tv = new Button(getActivity());
-        tv.setText("list");
-        return tv;
+        View view = inflater.inflate(R.layout.list_fragment, container,false);
+        return view;
     }
 }
--- app/src/main/java/fi/jessestenroth/koskaelokuvat/fragments/infoFragment.java
+++ app/src/main/java/fi/jessestenroth/koskaelokuvat/fragments/infoFragment.java
@@ -4,14 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import fi.jessestenroth.koskaelokuvat.R;
 
 public class infoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceBundle){
-        Button tv = new Button(getActivity());
-        tv.setText("info");
-        return tv;
+        View view = inflater.inflate(R.layout.info_fragment, container,false);
+        return view;
     }
 }
