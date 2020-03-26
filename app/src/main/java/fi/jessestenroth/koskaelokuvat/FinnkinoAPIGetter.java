54b8784976e6678b6492f829c06d661977d9af48
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-03-26 13:30:15 +0200

    add some xml reading

--- app/src/main/java/fi/jessestenroth/koskaelokuvat/FinnkinoAPIGetter.java
+++ app/src/main/java/fi/jessestenroth/koskaelokuvat/FinnkinoAPIGetter.java
@@ -7,6 +7,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -38,6 +39,32 @@ public class FinnkinoAPIGetter {
                 String name = ((Node) nameList.item(0)).getNodeValue();
 
                 lista.add(new area(id, name));
+            }
+            return lista;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList<String> getTimes(String code){
+        try{
+            ArrayList<String> lista = new ArrayList<>();
+            URL url = new URL("https://www.finnkino.fi/xml/ScheduleDates//?area=" + code);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new InputSource(url.openStream()));
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("dateTime");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                String aika = node.getNodeValue();
+                String[] splits = aika.split("T");
+                String day = "" + splits[0].charAt(8) + splits[0].charAt(9);
+                String month = "" + splits[0].charAt(5) + splits[0].charAt(6);
+                String year = "" + splits[0].charAt(0) + splits[0].charAt(1) + splits[0].charAt(2) + splits[0].charAt(3);
+                lista.add(day + "." + month + "." + year);
             }
             return lista;
         } catch (Exception e){
--- app/src/main/java/fi/jessestenroth/koskaelokuvat/fragments/searchFragment.java
+++ app/src/main/java/fi/jessestenroth/koskaelokuvat/fragments/searchFragment.java
@@ -4,33 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.fragment.app.Fragment;
+import fi.jessestenroth.koskaelokuvat.FinnkinoAPIGetter;
 import fi.jessestenroth.koskaelokuvat.R;
+import fi.jessestenroth.koskaelokuvat.area;
 
 public class searchFragment extends Fragment {
+    private Spinner aikaa;
+    private List<area> help;
+    private FinnkinoAPIGetter xml = new FinnkinoAPIGetter();
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceBundle){
         View view = inflater.inflate(R.layout.search_fragment, container,false);
-        Spinner aikaa = (Spinner) view.findViewById(R.id.aikaSpinner);
+        aikaa = (Spinner) view.findViewById(R.id.aikaSpinner);
         Spinner paikkaa = (Spinner) view.findViewById(R.id.paikkaSpinner);
-        ArrayList<String> ajat = new ArrayList<>();
         ArrayList<String> paikat = new ArrayList<>();
-        ajat.add("122.224.44141");
-        ajat.add("2.5.1234");
-        ajat.add("56.23.1345");
-        paikat.add("Tampere");
-        paikat.add("Helsinki");
-        paikat.add("Espoo");
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, ajat);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        aikaa.setAdapter(arrayAdapter);
+        help = xml.getAreas();
+        for(int lap=0; lap < help.size(); lap++){
+            paikat.add(help.get(lap).getName());
+        }
         ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, paikat);
         arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         paikkaa.setAdapter(arrayAdapter2);
+
+        aikaa.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                String code = help.get(position).getId();
+                ArrayList<String> ajat = xml.getTimes(code);
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, ajat);
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                aikaa.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
         return view;
     }
 }
