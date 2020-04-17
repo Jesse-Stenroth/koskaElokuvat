f01caf4567f79d4096e57401fa838a86f52e779e
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-04-17 15:10:59 +0300

    change: fix

--- app/src/main/java/fi/jessestenroth/koskaelokuvat/FinnkinoAPIGetterSettings.java
+++ app/src/main/java/fi/jessestenroth/koskaelokuvat/FinnkinoAPIGetterSettings.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
