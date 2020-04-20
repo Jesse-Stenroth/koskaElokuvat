2c54186dffd3bca0090084ec91cc39c68408e821
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-04-20 12:25:59 +0300

    change: comments to SettingsActivity

--- app/src/main/java/fi/jessestenroth/koskaelokuvat/SettingsActivity.java
+++ app/src/main/java/fi/jessestenroth/koskaelokuvat/SettingsActivity.java
@@ -18,15 +18,24 @@ import android.widget.Switch;
 
 import java.util.Locale;
 
+/**
+ * This activity is for app Settings. There user can change settings
+ * @author Jesse Stenroth
+ */
 public class SettingsActivity extends AppCompatActivity implements FinnkinoAPIGetterSettings.getDataToSettings {
+    //hold app language options
     private Spinner lang;
+    //hold areas options
     private Spinner area;
+    //save functions
     private SavingFeature save;
+    //gps on/off
     private Switch select;
     //because area listener call functions also when spinner is created
     private boolean canSaveArea = false;
     //because listener call also when spinner create
     private boolean canRun = false;
+    //debug feature
     private boolean debug = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,7 @@ public class SettingsActivity extends AppCompatActivity implements FinnkinoAPIGe
         this.area = (Spinner) findViewById(R.id.areas_settings);
         this.select = (Switch) findViewById(R.id.switch1);
 
+        //get data of area options
         FinnkinoAPIGetterSettings xml = new FinnkinoAPIGetterSettings(area, this, this);
         xml.getAreas();
         this.lang.setSelection(-1);
@@ -50,6 +60,7 @@ public class SettingsActivity extends AppCompatActivity implements FinnkinoAPIGe
                     if(value.equals("Finnish") || value.equals("Suomi")){
                         setLang("fi");
                     } else if(value.equals("System") || value.equals("Järjestelmä")){
+                        //this set language same as device
                         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
                             if (debug) {
                                 Log.e("locale", Resources.getSystem().getConfiguration().getLocales().get(0).getLanguage());
@@ -92,8 +103,14 @@ public class SettingsActivity extends AppCompatActivity implements FinnkinoAPIGe
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        //view can update
         save.saveBoolean("update", true);
     }
+
+    /**
+     * This method set language for app. ( example 'en' means app language is english)
+     * @param l short version of language what app use
+     */
     private void setLang(String l){
         Resources resources = getResources();
         DisplayMetrics dm = resources.getDisplayMetrics();
