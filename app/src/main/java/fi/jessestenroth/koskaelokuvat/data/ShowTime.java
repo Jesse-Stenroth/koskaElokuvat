5d5d551e82441533f034b3cc02a98d0d33daf2cf
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-04-20 13:12:24 +0300

    change: comments to ShowTime

--- app/src/main/java/fi/jessestenroth/koskaelokuvat/data/ShowTime.java
+++ app/src/main/java/fi/jessestenroth/koskaelokuvat/data/ShowTime.java
@@ -1,7 +1,9 @@
 package fi.jessestenroth.koskaelokuvat.data;
 
-import java.util.List;
-
+/**
+ * This class is object what hold data of one showtime
+ * @author Jesse Stenroth
+ */
 public class ShowTime {
     private int ID;
     private String ratingImage;
@@ -14,6 +16,19 @@ public class ShowTime {
     private String EventSmallImagePortrait;
     private String Ticket;
 
+    /**
+     * This constructor set all data to object
+     * @param ID showtime id
+     * @param ratingImage rating image url
+     * @param start start time
+     * @param end end time
+     * @param eventID showtime event id
+     * @param title title of movie
+     * @param theathreID theatre where showtime is
+     * @param theatreAndAuditorium auditorium where show movie
+     * @param eventSmallImagePortrait small image of movie
+     * @param ticketBuy ticket buying url
+     */
     public ShowTime(int ID, String ratingImage, String start, String end, int eventID, String title, String theathreID, String theatreAndAuditorium, String eventSmallImagePortrait, String ticketBuy) {
         this.ID = ID;
         this.ratingImage = ratingImage;
@@ -26,6 +41,11 @@ public class ShowTime {
         this.Ticket = ticketBuy;
     }
 
+    /**
+     * This method set start and end times to showtime
+     * @param start starting time
+     * @param end ending time
+     */
     private void setStartAndEnd(String start, String end) {
         String[] starts = start.split("T");
         String[] ends = end.split("T");
@@ -33,42 +53,82 @@ public class ShowTime {
         this.start = starts[1];
     }
 
+    /**
+     * This method return ticket buying url
+     * @return url of ticket
+     */
     public String getTicket() {
         return Ticket;
     }
 
+    /**
+     * This method return showtime id
+     * @return showtime id
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     * This method return image url
+     * @return rating image url
+     */
     public String getRatingImage() {
         return ratingImage;
     }
 
+    /**
+     * This method return start time
+     * @return movie start
+     */
     public String getStart() {
         return start;
     }
 
+    /**
+     * This method return end time
+     * @return movie end
+     */
     public String getEnd() {
         return end;
     }
 
+    /**
+     * This method return event id
+     * @return event id
+     */
     public int getEventID() {
         return eventID;
     }
 
+    /**
+     * This method return title of movie
+     * @return movie title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * This method return theaatre id
+     * @return id where showtime is
+     */
     public String getTheathreID() {
         return theathreID;
     }
 
+    /**
+     * This method return details where movie show
+     * @return auditorium where movie is
+     */
     public String getTheatreAndAuditorium() {
         return TheatreAndAuditorium;
     }
 
+    /**
+     * Small image of movie
+     * @return image url of movie
+     */
     public String getEventSmallImagePortrait() {
         return EventSmallImagePortrait;
     }
